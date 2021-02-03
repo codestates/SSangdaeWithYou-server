fb0fe9ec8829dcea44df97bfe9e73d0b37c0b117
Hyunsoo-dev <jing07161@gmail.com>
2021-02-03 20:13:20 +0900

    test server

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  //console.log(port + '접속11111');
-  res.send(port + '접속11111');
+  res.send('첫 방문 축하드립니다.');
 })
 
 app.listen(port, () => {
