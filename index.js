f1536f4e77d9d1288cc2dcd7ac3a3d4cab348ed6
Hyunsoo-dev <jing07161@gmail.com>
2021-02-03 20:48:30 +0900

    cors 설정 추가

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('첫 방문 축하드립니다.');
+  res.send({data: '첫 방문 축하드립니다.'});
 })
 
 app.listen(port, () => {
