b0e0492ba6d8409b8d830d1db884bd4da7b54f89
Hyunsoo-dev <jing07161@gmail.com>
2021-02-04 11:30:15 +0900

    [SERVER] [ADD] https 서버 구현

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 const port = 5000;
 
 app.use(cors());
@@ -9,6 +11,14 @@ app.get('/', (req, res) => {
   res.send({data: '첫 방문 축하드립니다.'});
 })
 
-app.listen(port, () => {
-  console.log(port + '접속');
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync(__dirname + '/key.pem', 'utf-8'),
+      cert: fs.readFileSync(__dirname + '/cert.pem', 'utf-8'),
+    },
+    app.use('/', (req, res) => {
+      res.send('Congrats! You made https server now :)');
+    })
+  ).listen(port);
+
