40c091a9cd25349696e6588f492915a21e83a08a
Ubuntu <ubuntu@ip-172-31-34-213.ap-northeast-2.compute.internal>
2021-02-11 14:45:23 +0000

    [SERVER] [FIX] 오류 수정

--- controllers/list/placeUpload.js
+++ controllers/list/placeUpload.js
@@ -2,6 +2,7 @@ const { smokePlace, user } = require('../../models');
 
 module.exports = {
   placeUpload: async (req, res) => {
+	  console.log(req.cookies);
     if (!req.cookies.id) {
       res.sendStatus(400);
     } else {
@@ -12,26 +13,26 @@ module.exports = {
           username: username,
         },
       });
-      await smokePlace
+
+	    console.log(userId);
+         smokePlace
         .findOrCreate({
           where: {
             longitude: longitude,
             latitude: latitude,
           },
           defaults: {
-            userId: userId,
+		  userId: userId.dataValues.id,
             comment: comment,
             placeName: placeName,
           },
         })
-        .then(async ([smokeplace, created]) => {
+        .then(([smokeplace, created]) => {
           if (!created) {
             return res.status(400).send('이미 추가된 장소입니다');
           } else {
             res.status(200).send('리스트가 추가되었습니다');
           }
-          const data = await smokeplace.get({ plain: true });
-          res.status(200).send('리스트가 추가되었습니다');
         })
         .catch((err) => res.status(400).send('입력값이 잘못되었습니다.'));
     }
