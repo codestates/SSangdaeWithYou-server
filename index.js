638ad030267c62ff93fa04ea321d73a8d0571e83
선종권 <jonggwon@seonjong-gwon-ui-MacBookPro.local>
2021-02-11 21:20:33 +0900

    [SERVER] [FIX] controller 쿠키로 수정

--- index.js
+++ index.js
@@ -5,39 +5,40 @@ const https = require('https');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const port = 5000;
 const route = require('./routes/index');
-
+const cookieParser = require('cookie-parser');
 
 // express-session
-app.use(
-  session({
-    secret: '@codestates',
-    resave: false,
-    saveUninitialized: true, // false 로 설정해주면 cookie가 오지 않음
-    cookie: {
-      domain: 'ssangdae.gq',
-      path: '/',
-      maxAge: 24 * 6 * 60 * 10000,
-      sameSite: 'none',
-      httpOnly: true,  
-      secure: true  //https
-    },
-  })
-)
+// app.use(
+//   session({
+//     secret: '@codestates',
+//     resave: false,
+//     saveUninitialized: true, // false 로 설정해주면 cookie가 오지 않음
+//     cookie: {
+//       domain: 'ssangdae.gq',
+//       path: '/',
+//       maxAge: 24 * 6 * 60 * 10000,
+//       sameSite: 'none',
+//       httpOnly: true,
+//       secure: true  //https
+//     },
+//   })
+// )
+app.use(cookieParser);
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:443",
-    "https://localhost:443",
-    "https://localhost:3000"
-  ],
-  credentials: true
-}));
-
-
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://localhost:3000',
+      'https://ssangdaewithyou.com',
+    ],
+    methods: true,
+    credentials: true,
+  })
+);
 
 // process.on('uncaughtException', (err) => {console.log(err)})
 app.use('/', route);
