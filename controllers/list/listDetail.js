e0d15f48023fe2e26a02a4968b65945f73f7bb9e
Hyunsoo-dev <jing07161@gmail.com>
2021-02-13 20:19:32 +0900

    [SERVER] [FIX] 코드 수정

--- controllers/list/listDetail.js
+++ controllers/list/listDetail.js
@@ -85,7 +85,7 @@ module.exports = {
             attributes: ['message'],
             where: { smokePlaceId: placeId },
           },
-          order: [[ {model: message}, 'createdAt', 'DESC']],
+          order: [[ message, 'createdAt', 'DESC']],
         })
         .catch((err) => res.sendStatus(500));
       console.log(result);
